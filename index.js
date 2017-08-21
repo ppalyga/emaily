2d59fb7ae49314f5841869916ae18172009a6168
ppalyga <homme.ex.machina@gmail.com>
2017-08-21 19:23:51 +0200

    ADDED production variables

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //Podstawowe importy: express, mongoose, plik z kluczami konfiguracyjnymi, model użytkownika (musi być zaimportowany pierwszy, bo jest użyty w passport.js), passport.
 const express = require('express'),
   mongoose = require('mongoose'),
+  cookieSession = require('cookie-session'),
+  passport = require('passport'),
   keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -8,11 +10,24 @@ require('./services/passport');
 //Połączenie z bazą danych.
 mongoose.connect(keys.mongoURI);
 
-//Stworzenie instncji aplikacji, import routów z zewnętrznego pliku.
+//Stworzenie instncji aplikacji.
 app = express();
+
+//Konfiguracja ciasteczek dla sesji.
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Import routów z zewnętrznego pliku (po konfiguracji ciaseczek).
 require('./routes/authRoutes')(app);
 
-//Route główny. 
+//Route główny.
 app.get('/', (req, res) => {
   res.send('This is the homepage.');
 });
